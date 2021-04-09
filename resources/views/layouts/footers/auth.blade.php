5a37c123a4faf8c1157e958d36c0881719bca3a4
habibaboujmil <boujmilhabiba1995@gmail.com>
2021-04-09 23:48:16 +0100

    first commit

---
+++ resources/views/layouts/footers/auth.blade.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+    <div class="container">
+        <div class="copyright float-right">
+        &copy;
+        <script>
+            document.write(new Date().getFullYear())
+        </script> - SECCA, Développé par
+        <a href="https://www.linkedin.com/in/habiba-boujmil-121774139/" target="_blank">Habiba BOUJMIL</a> 
+        </div>
+    </div>
+</footer>
